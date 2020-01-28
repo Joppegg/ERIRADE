bee87c1bc9ee13214c7c70469e2555225fd6fcfa
Jon <jo0748gr-s@student.lu.se>
2020-01-28 16:12:41 +0100

    basic

--- erirad/src/components/ReportSubmit.js
+++ erirad/src/components/ReportSubmit.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 
 function ReportSubmit(props) {
+
+const [report, setReport] = useState({
+    name: '',
+    text: ''
+})
+
     return (
-           
-          
-                <div className="main-column">
-                <h2>Main Column</h2>
-                <p>Här är massor med text</p>
-                </div>
+        <div className="main-column">
+            <form>
+                <label>Name</label>
+                <input type = "text"
+                value= {report.name}
+                onChange = { e => setReport({...report, name: e.target.value})}
+                />
+                <label> Report </label>
+                <textarea id="subject" name="subject" placeholder="write something">
+
+
+                </textarea>
+
+                <input type="submit" value="submit"/>
+            
+            </form>
+        </div>
 
     );
 }
