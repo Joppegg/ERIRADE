2cd505eb601556cec2ea7052a8bcbde0a26a5801
Jon <jo0748gr-s@student.lu.se>
2020-01-28 15:48:43 +0100

    Added base ui and flexbox

---
+++ erirad/src/components/MainView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SideView from './SideView';
+import ReportSubmit from './ReportSubmit';
+
+function MainView(props) {
+    return (
+         <div className="column-layout">
+         <SideView/>
+         <ReportSubmit/>
+        </div>
+    );
+}
+
+export default MainView;
