869843a2cde2361cd3171fff7aebc448b4cb4a62
Johan Erdös <johan.erdos@ericsson.com>
2020-01-29 13:06:44 +0100

    fetch

--- erirad/src/php/SnippetPost.php
+++ erirad/src/php/SnippetPost.php
@@ -1,5 +1,13 @@
 <?php 
-
+header("Access-Control-Allow-Origin: *");
 require 'DBConnection.php';
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
 
+$snippetText = mysqli_escape_string($connection, isset($_POST['text']));
+
+$sql = "INSERT INTO snippet (snippetText) VALUES ('$snippetText')";
+
+
+mysqli_query($connection, $sql);
 ?>
