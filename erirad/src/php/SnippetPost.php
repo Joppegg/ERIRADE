c4140b8eae401d5bd0f96419661a33012dae113f
Johan Erdös <johan.erdos@ericsson.com>
2020-02-03 18:23:25 +0100

    stash

--- erirad/src/php/SnippetPost.php
+++ erirad/src/php/SnippetPost.php
@@ -10,14 +10,10 @@ $_POST = json_decode($rest_json, true);
 $snippets = json_decode(json_encode($_POST['textValues']));
 $tags = json_decode(json_encode($_POST['tags']));
 
-echo $snippets[0];
-echo $snippets[1];
-
 foreach($snippets as $snippetText){
-    echo $snippetText;
-    $reportId = '11';
-        
     
+    $reportId = '11';
+  
     $sqlInsertSnippet = "INSERT INTO snippet (reportId, snippetText) VALUES ('$reportId','$snippetText')";
     mysqli_query($connection, $sqlInsertSnippet);
 
@@ -32,16 +28,7 @@ foreach($snippets as $snippetText){
     }
     
     
-    /*
-    $sqlGetReportId = "SELECT reportId FROM report WHERE employeeId IS NULL";
-
-    $resultReportId = mysqli_query($connection, $sqlGetReportId);
-    $resultCheckReportId = mysqli_num_rows($resultReportId);
-
     
-    $row = mysqli_fetch_array($resultReportId);
-    $reportId = $row['reportId'];
-    */
     
 
 }
