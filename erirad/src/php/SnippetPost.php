aa2ffa2f9a42071c152350b6b1b70fad2d67b186
Johan Erdös <johan.erdos@ericsson.com>
2020-02-03 12:11:34 +0100

    matrix

--- erirad/src/php/SnippetPost.php
+++ erirad/src/php/SnippetPost.php
@@ -1,3 +1,4 @@
+
 <?php 
 header("Access-Control-Allow-Origin: *");
 require 'DBConnection.php';
@@ -8,6 +9,15 @@ $_POST = json_decode($rest_json, true);
 $snippets = json_decode($_GET['textValues']);
 //$employeeId = mysqli_escape_string($_POST, ['employeeId']);
 
+/*
+$snippetID = mysqli_escape_string($_POST['snippetId']);
+$snippets = array(
+    array($snippetID, 'albin'),
+    array('1', 'johan')
+);
+*/
+echo $snippets[0][0].": SnippetID" .$snippets[0][1]. ": Snippet Text";
+echo $snippets[1][0].": SnippetID" .$snippets[1][1]. ": Snippet Text";
 
     $snippetId = $snippets[0];
     $snippetText = $snippets[1];
@@ -20,10 +30,12 @@ $snippets = json_decode($_GET['textValues']);
     if($resultCheckReportId > 1){
         $row = mysqli_fetch_array($resultReportId);
         $reportId = $row['reportId'];
+
+        $sqlInsertSnippet = "INSERT INTO snippet (snippetId, reportId, snippetText) VALUES ('$snippetId','$reportId','$snippetText')";
+        mysqli_query($connection, $sqlInsertSnippet);
     }
 
-    $sqlInsertSnippet = "INSERT INTO snippet (snippetId, reportId, snippetText) VALUES ('$snippetId','$reportId','$snippetText')";
-    mysqli_query($connection, $sqlInsertSnippet);
+   
     
 
 
