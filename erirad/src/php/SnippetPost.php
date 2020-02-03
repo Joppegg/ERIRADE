d5359fc794d4c5960421c0655340db65b86ef96b
Johan Erdös <johan.erdos@ericsson.com>
2020-02-03 13:16:24 +0100

    bugfix

--- erirad/src/php/SnippetPost.php
+++ erirad/src/php/SnippetPost.php
@@ -7,38 +7,31 @@ $_POST = json_decode($rest_json, true);
 
 
 
-$snippets = ($_POST['testArray']);
+$snippets = json_decode(json_encode($_POST['textValues']));
 
-//$employeeId = mysqli_escape_string($_POST, ['employeeId']);
 
-/*
-$snippetID = mysqli_escape_string($_POST['snippetId']);
-$snippets = array(
-    array($snippetID, 'albin'),
-    array('1', 'johan')
-);
-*/
-echo $snippets[0];
-echo $snippets[1];
-/*
-echo $snippets[0][0].": SnippetID" .$snippets[0][1]. ": Snippet Text";
-echo $snippets[1][0].": SnippetID" .$snippets[1][1]. ": Snippet Text";
-*/
-    $snippetId = $snippets[0];
-    $snippetText = $snippets[1];
 
+foreach($snippets as $snippetText){
+    
+    
     $sqlGetReportId = "SELECT reportId FROM report WHERE employeeId IS NULL";
 
     $resultReportId = mysqli_query($connection, $sqlGetReportId);
     $resultCheckReportId = mysqli_num_rows($resultReportId);
 
-    if($resultCheckReportId > 1){
-        $row = mysqli_fetch_array($resultReportId);
-        $reportId = $row['reportId'];
+    
+    $row = mysqli_fetch_array($resultReportId);
+    $reportId = $row['reportId'];
+    
+        
+    
+    $sqlInsertSnippet = "INSERT INTO snippet (reportId, snippetText) VALUES ('$reportId','$snippetText')";
+    mysqli_query($connection, $sqlInsertSnippet);
 
-        $sqlInsertSnippet = "INSERT INTO snippet (snippetId, reportId, snippetText) VALUES ('$snippetId','$reportId','$snippetText')";
-        mysqli_query($connection, $sqlInsertSnippet);
-    }
+}
+    
+
+    
 
    
     
