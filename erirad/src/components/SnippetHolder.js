8a1237de9d3b1f335bb5e180e3d670358b8a61f3
Jon <jo0748gr-s@student.lu.se>
2020-02-04 19:33:09 +0100

    Added a start to the homepage, to fetch tags.

--- erirad/src/components/SnippetHolder.js
+++ erirad/src/components/SnippetHolder.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import '../css/Header.css'
 import Snippet from './Snippet';
 import Fab from '@material-ui/core/Fab';
@@ -10,7 +10,7 @@ import {
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 /*
 *This functions purpose is to dynamically render and pass state to the child components, which will be individual snippets.
 */
@@ -22,35 +22,33 @@ function SnippetHolder(props) {
     const [snippetTextArea, setSnippetTextArea] = useState([{}]);
 
     const [tags, setTags] = useState({
-        
+
     })
-     
-  
+
+
 
     //Uppdaterar state med texterna.
-    const handleFieldChange = (fieldId, value) =>{
-        setTextValues({...textvalues, [fieldId]: value});
+    const handleFieldChange = (fieldId, value) => {
+        setTextValues({ ...textvalues, [fieldId]: value });
     };
-    const handleTagChange = (tagId, value) => 
-    { 
-       setTagValues({...tagValues, [tagId] : value})
-       //Tag id vill vi ha kvar.
-       setTags({...tags, [tagId]: value})
+    const handleTagChange = (tagId, value) => {
+        setTagValues({ ...tagValues, [tagId]: value })
+        //Tag id vill vi ha kvar.
+        setTags({ ...tags, [tagId]: value })
     };
 
-    const handleTagId = (tagId, value) => 
-    { 
-        setTags({...tags, [tagId] : value })
+    const handleTagId = (tagId, value) => {
+        setTags({ ...tags, [tagId]: value })
     };
 
     const newSnippets = snippetTextArea.map((snippet, index) => (
-          <Snippet
-          key={index}
-          id={index}
-          onChange={handleFieldChange}
-          onTagId={handleTagId}
-          onTagChecked={handleTagChange}
-          value={textvalues[snippet]}
+        <Snippet
+            key={index}
+            id={index}
+            onChange={handleFieldChange}
+            onTagId={handleTagId}
+            onTagChecked={handleTagChange}
+            value={textvalues[snippet]}
         />
     ));
 
@@ -60,33 +58,29 @@ function SnippetHolder(props) {
     }
 
     const handleSubmit = () => {
-       // const testArray = {testArray: ["1", "hej"]}
-        const namedList = JSON.stringify({textValues: textvalues}, null, 2)
-        const matrix = JSON.stringify({matrix: tags}, null, 2)
-
         const payload = {
             textValues: textvalues,
             matrix: tags
         }
-        
+
         axios({
-          method: 'post',
-          url: `http://localhost/ERIRADAPP/erirad/src/php/SnippetPost.php`,
-          headers: { 'content-type': 'application/json' },
-          data: JSON.stringify(payload, null, 2)
-            
+            method: 'post',
+            url: `http://localhost/ERIRADAPP/erirad/src/php/SnippetPost.php`,
+            headers: { 'content-type': 'application/json' },
+            data: JSON.stringify(payload, null, 2)
+
         })
-          .then(result => {
-          console.log(result.data)
-         
-          })
-          .catch(error => console.log(error));
+            .then(result => {
+                console.log(result.data)
+
+            })
+            .catch(error => console.log(error));
 
     }
 
     return (
         <div>
-        <Link to="/home">Home</Link>
+            <Link to="/">Home</Link>
             <div className="snippetContainer">{newSnippets}</div>
             <div className="add-container">
                 <div className="add-space"></div>
@@ -101,13 +95,13 @@ function SnippetHolder(props) {
                     variant="contained"
                     color="primary"
                     onClick={handleSubmit}
-                 >
+                >
                     Submit Report
                        </Button>
             </div>
             <pre>{JSON.stringify(tags, null, 2)}</pre>
             <pre>{/*JSON.stringify(textvalues, null, 2)*/}</pre>
-            <pre>{/*JSON.stringify(tagValues, null, 2)*/}</pre>   
+            <pre>{/*JSON.stringify(tagValues, null, 2)*/}</pre>
         </div>
     );
 }
--- erirad/src/components/UserHomePage.js
+++ erirad/src/components/UserHomePage.js
@@ -4,15 +4,30 @@ import {
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
+import TagSelector from './TagSelector';
+
+
 //This function will serve as the main view when the user is logged in
 function UserHomePage(props) {
 
     const [employeeId, setEmployeeId] = useState(0);
 
     return (
-         <div className="snippetContainer">
-         <Link to="/">Your report view</Link>
+        <div className="snippetContainer">
+            <div className="homepageContainer">
+                <div className="homepageRow">
+                       <Link to="/report">Your report view</Link>
+                </div>
+
+                <div className="homepageRow">
+                       <h2>Filter on what kind of information you would like to see here!</h2>
+                </div>
+                <div className="homepageRow">
+                       <TagSelector/>
+                </div>
+
+            </div>
         </div>
     );
 }
