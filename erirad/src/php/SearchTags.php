32f064317507b7e0dd0de3130ba88335f0e7d073
Jon <jo0748gr-s@student.lu.se>
2020-02-05 19:58:22 +0100

    Now working as intended, removed mocking post request in php..

--- erirad/src/php/SearchTags.php
+++ erirad/src/php/SearchTags.php
@@ -4,18 +4,11 @@ require 'DBConnection.php';
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
-//$selectedTags = json_decode(json_encode($_POST['selectedTags']));
-$selectedTags = array(1, 2);
+$selectedTags = json_decode(json_encode($_POST['tags']));
 
-// foreach selectedTag:
-// Select snippetId from snippetTag where tagId = $selectedTags
-//     foreach snippetId
-//        select * from Snippet where SnippetId = snippetId
-//          echo (snippetId, snippetText)
-//
-//
-//
-//
+
+
+
 $snippetIdAndText = array();
 foreach($selectedTags as $tagId){
 
@@ -24,8 +17,6 @@ foreach($selectedTags as $tagId){
 
     while($rowSnippetId = mysqli_fetch_array($resultSnippets)){
 
-    //    echo(json_encode( $tagId . ":". $rowSnippetId));
-       // echo(json_encode($rowSnippetId['snippetid']));
         $snippetSpecificId = $rowSnippetId['snippetid'];
 
         $sqlGetSnippetTextAndId = "SELECT snippetId, snippetText FROM snippet WHERE snippetId ='$snippetSpecificId'";
@@ -37,25 +28,7 @@ foreach($selectedTags as $tagId){
 
 
 
-       
-
-
-        /*
-        foreach($rowSnippetId as $snippetId){
-         
-            $sqlGetSnippetText = "SELECT * FROM snippet WHERE snippetId = '$snippetId'";
-            $resultSnippetText = mysqli_query($connection, $sqlGetSnippetText);
-            
-            while($rowSnippetText = mysqli_fetch_array($resultSnippetText)){
-                /*
-                foreach($rowSnippetText as $snippetText){
-                    echo json_encode("Snippet ID: " .$snippetId. " ");
-                    echo json_encode("Text: " .$snippetText. " ");
-                }
-               
-              //  echo(json_encode($rowSnippetText));
-            }
-       }*/  
+    
     }
 }
 
