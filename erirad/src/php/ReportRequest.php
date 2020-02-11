cd7a06c3413bf39732316961e2565e8f19606110
Johan Erdös <johan.erdos@ericsson.com>
2020-02-11 17:26:05 +0100

    empInput

--- erirad/src/php/ReportRequest.php
+++ erirad/src/php/ReportRequest.php
@@ -5,6 +5,10 @@ $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
 $report = (array)json_decode(json_encode($_POST['report']));
+$listOfEmployees = $report['employees'];
+//problem: tar endast det första empId - måste loopa igenom men då kan det bli knas
+$empId = reset($listOfEmployees)->employeeId;
+
 
 $authorId = mysqli_escape_string($connection, $_POST['requesterId']);
 $title = $report['title'];
@@ -18,7 +22,6 @@ $sqlInsertReportRequest = "INSERT INTO reportrequest (authorId, title, descripti
 mysqli_query($connection, $sqlInsertReportRequest);
 
 //notice for that a report has been requested from a specific user 
-
 $sqlGetRequest = "SELECT * FROM reportrequest WHERE authorId = '$authorId'";
 $resultRequest = mysqli_query($connection, $sqlGetRequest);
 $numberOfRows = mysqli_num_rows($resultRequest);
@@ -26,8 +29,30 @@ $numberOfRows = mysqli_num_rows($resultRequest);
 if($numberOfRows > 0){
     while($row = mysqli_fetch_assoc($resultRequest)){
         $requests[] = $row;
+        $requestId = $row['requestId'];
         echo json_encode($requests);
     }
 }
+
+foreach($listOfEmployees as $employee){
+    $submitted = "false";
+    $accepted = "false";
+    $submittedDate = "null";
+
+    //inserting into report to generate a reportId :)
+    $submitted = "no";
+    $sqlCreateNewReport = "INSERT INTO report (employeeId, submitted) VALUES ('$empId', '$submitted')";
+    mysqli_query($connection, $sqlCreateNewReport);
+
+    //getting the generated reportId to be used as FK in employeeInput
+    $sqlGetReportId = "SELECT reportId FROM report WHERE employeeId = '$empId' ORDER BY reportId DESC LIMIT 1";
+    $resultReportId = mysqli_query($connection, $sqlGetReportId);
+    $list = mysqli_fetch_assoc($resultReportId);
+    $reportId = $list['reportId'];
+    $sqlInsertEmployeeInput = "INSERT INTO employeeinput (employeeid, requestid, reportid, submitted, accepted, submittedDate) VALUES ('$empId', '$requestId', '$reportId', '$submitted', '$accepted', '$submittedDate')";
+    mysqli_query($connection, $sqlInsertEmployeeInput);
+    }
+
+
 
 
