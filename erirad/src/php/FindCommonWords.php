afc14fdaf4dd6c59180585cb3375bdf6a857c795
Johan Erdös <johan.erdos@ericsson.com>
2020-02-11 12:13:47 +0100

    pushing

--- erirad/src/php/FindCommonWords.php
+++ erirad/src/php/FindCommonWords.php
@@ -9,13 +9,13 @@ $_POST = json_decode($rest_json, true);
 $sqlGetSnippetText = "SELECT snippettext FROM snippet";
 $resultSnippetTexts = mysqli_query($connection, $sqlGetSnippetText);
 
-while($row = mysqli_fetch_array($resultSnippetTexts)){
+while($row = mysqli_fetch_assoc($resultSnippetTexts)){
     $textlist[] = $row['snippettext'];
     foreach($textlist as $snippetText){
         $ary = explode(' ',$snippetText);
         foreach($ary as $text){
             $stop_words = array("a", "you", "if", "the", "this", "that", "about", "entry", "report", "have", "with", "is", "where", "attended", "meetings", "discussed", "multiple", "snippets");
-            $max_count = 10;
+            $max_count = 100;
             $string = preg_replace('/ss+/i', '', $text);
             $string = trim($string); // trim the string
             $string = preg_replace('/[^a-zA-Z -]/', '', $text); // only take alphabet characters, but keep the spaces and dashes too…
@@ -33,15 +33,16 @@ while($row = mysqli_fetch_array($resultSnippetTexts)){
             $frequency = array_count_values($word_count);
             arsort($frequency);
         
-            //arsort($word_count_arr);
+           
             $keywords = array_slice(array_keys($frequency), 0, $max_count);
+            
             if(sizeof($keywords) != 0){
-                $count = max($keywords);
-                echo $count;
-                if($count > 1){
-                    echo $count[0];
+                $keywordsUnique = array_unique($keywords);
+                var_dump($keywordsUnique);
+                $count = 10;
+                if($count > 100){
                     
-                    $sqlInsertNewTag = "INSERT INTO tag (tagName) VALUES ($tagName)";
+                    $sqlInsertNewTag = "INSERT INTO tag (tagName) VALUES ($count)";
                     mysqli_query($connection, $sqlInsertNewTag);
                 }else{
                 //echo 'word has not been used enough times'; 
@@ -53,6 +54,4 @@ while($row = mysqli_fetch_array($resultSnippetTexts)){
 
     }
     
-
-//}
 }
