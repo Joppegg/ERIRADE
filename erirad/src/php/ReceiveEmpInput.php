4c1a984e256798dfd2b3010f05ec451cd7d64a63
Jon <jo0748gr-s@student.lu.se>
2020-02-12 18:16:25 +0100

    changed null reference

--- erirad/src/php/ReceiveEmpInput.php
+++ erirad/src/php/ReceiveEmpInput.php
@@ -5,11 +5,12 @@ $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
 
-//$empId = mysqli_escape_string($connection, $_POST['employeeId']);
+$empId = mysqli_escape_string($connection, $_POST['employeeId']);
 //DENNA QUERY:
-$empId = '5';
+//$empId = '5';
 $sqlJoined = "SELECT e.employeeId, e.requestId, e.reportId, r.title, r.description, r.authorId FROM employeeinput e JOIN reportrequest r ON e.requestId = r.requestId WHERE e.employeeId = '$empId'";
 
+$allEmpInput = array();
 $resultJoined = mysqli_query($connection, $sqlJoined);
 while($rowEmpInput = mysqli_fetch_array($resultJoined)){
     $allEmpInput[] = $rowEmpInput;
