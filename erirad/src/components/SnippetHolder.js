ef2eb423b43768011a558fac9bde0f92db3af485
Jon <jo0748gr-s@student.lu.se>
2020-02-18 10:09:21 +0100

    state in createreport

--- erirad/src/components/SnippetHolder.js
+++ erirad/src/components/SnippetHolder.js
@@ -9,7 +9,7 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { green } from '@material-ui/core/colors';
-
+import { useSelector, useDispatch } from 'react-redux';
 
 
 const useStyles = makeStyles(theme => ({
@@ -51,6 +51,9 @@ const useStyles = makeStyles(theme => ({
 
 function SnippetHolder({ employeeId }) {
 
+    const report = useSelector(state => state.reportSelected);
+
+    const employee = useSelector(state => state.employee);
     const [textvalues, setTextValues] = useState([])
     const [tagValues, setTagValues] = useState({})
     const [snippetTextArea, setSnippetTextArea] = useState([{}]);
@@ -154,7 +157,11 @@ function SnippetHolder({ employeeId }) {
         <div>
 
             <div className="snippetContainer">
-                <h3 className="text-center"> When submitted, these snippets are saved to a single report with individual tagging for filtering. </h3>
+            <h3 className="text-center"> Author here
+                     </h3>
+                <h3 className="text-center"> {report.title}
+                     </h3>
+                     <h4 className="text-center">{report.description}</h4>
                 {newSnippets}</div>
             <div className="add-container">
                 <div className="add-space"></div>
