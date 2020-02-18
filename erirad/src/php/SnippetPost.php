b9eec5f7960049a547d46633a41da00032906f64
Johan Erdös <johan.erdos@ericsson.com>
2020-02-18 10:40:28 +0100

    kk

--- erirad/src/php/SnippetPost.php
+++ erirad/src/php/SnippetPost.php
@@ -8,35 +8,16 @@ $snippets = json_decode(json_encode($_POST['textValues']));
 $matrix = json_decode(json_encode($_POST['matrix']));
 $employeeId = mysqli_escape_string($connection, $_POST['employeeId']);
 
-
+$latestReportId = mysqli_escape_string($connection, $_POST['reportId']);
 echo json_encode("employee ID: " .$employeeId);
-
-
-//vill ju inte skapa en ny report, vill ju ha det reportId som är requestat i empInput
-//så jävla snygg query
-$sqlGetReportIdFromEmpInput = "SELECT reportId FROM employeeInput ORDER BY reportId DESC LIMIT 1";
-$result = mysqli_query($connection, $sqlGetReportIdFromEmpInput);
-$latestReportIdFromEmpInputList = mysqli_fetch_assoc($result);
-$latestReportIdFromEmpInput = $latestReportIdFromEmpInputList['reportId'];
-echo $latestReportIdFromEmpInput;
-
-
 
 foreach($snippets as $parentkey=> $snippetText){
     $saveKey = $parentkey;
     
-    /*
-    $getLatestReportId = "SELECT reportId FROM report ORDER BY reportId DESC LIMIT 1";
-    $resultLatest = mysqli_query($connection, $getLatestReportId);
-
-    $latestReportId = mysqli_fetch_assoc($resultLatest);
-    $reportId = $latestReportId['reportId'];
-    */
-    
-    $sqlInsertSnippet = "INSERT INTO snippet (reportId, snippetText) VALUES ('$latestReportIdFromEmpInput','$snippetText')";
+    $sqlInsertSnippet = "INSERT INTO snippet (reportId, snippetText) VALUES ('$latestReportId','$snippetText')";
     $resultInsertedSnippet = mysqli_query($connection, $sqlInsertSnippet);
 
-    $sqlFindSnippetId = "SELECT snippetId FROM snippet WHERE reportId = '$latestReportIdFromEmpInput'";
+    $sqlFindSnippetId = "SELECT snippetId FROM snippet WHERE reportId = '$latestReportId'";
     $resultSnippetId = mysqli_query($connection, $sqlFindSnippetId);
 
     while($row = mysqli_fetch_array($resultSnippetId)){
@@ -64,14 +45,14 @@ foreach($snippets as $parentkey=> $snippetText){
     //Check if the snippet is inserted - update row in report if it is
     if($resultInsertedSnippet){
         $reportSubmitted = 'true';
-        $sqlUpdateReport = "UPDATE report SET submitted = '$reportSubmitted' WHERE reportId = '$latestReportIdFromEmpInput'";
+        $sqlUpdateReport = "UPDATE report SET submitted = '$reportSubmitted' WHERE reportId = '$latestReportId'";
         $resultUpdated = mysqli_query($connection, $sqlUpdateReport);
 
         //update the employeeinput row if report is submitted
         if($resultUpdated){
             
             $empInputSubmitted = 'true';
-            $sqlUpdateEmpInput = "UPDATE employeeinput SET submitted = '$empInputSubmitted' WHERE reportId = '$latestReportIdFromEmpInput'";
+            $sqlUpdateEmpInput = "UPDATE employeeinput SET submitted = '$empInputSubmitted' WHERE reportId = '$latestReportId'";
             mysqli_query($connection, $sqlUpdateEmpInput);
         }
     }
