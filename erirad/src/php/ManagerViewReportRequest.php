cf21f4e6e7388be51852a1f81b732cae46cbbc09
Johan Erdös <johan.erdos@ericsson.com>
2020-02-19 12:10:16 +0100

    rickard

--- erirad/src/php/ManagerViewReportRequest.php
+++ erirad/src/php/ManagerViewReportRequest.php
@@ -3,14 +3,32 @@ header("Access-Control-Allow-Origin: *");
 require 'DBConnection.php';
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
-
-$empId = mysqli_escape_string($connection, $_POST['employeeId']);
-//$empId = '5';
+$submittedTrue = 'true';
+$submitted = '';
+//$empId = mysqli_escape_string($connection, $_POST['employeeId']);
+$empId = '5';
 $sqlGetReportRequestInput = "SELECT requestId, title, description, creationTime FROM reportrequest WHERE authorId = '$empId'";
 $resultReportRequest = mysqli_query($connection, $sqlGetReportRequestInput);
 
 while($rowReportRequest = mysqli_fetch_array($resultReportRequest)){
     $formatListReportRequest[] = $rowReportRequest;
+    $requestId = $rowReportRequest['requestId'];
+
+    $sqlAllRequestsSubmitted = "SELECT COUNT (*) AS count FROM employeeinput WHERE submitted != '$submittedTrue' AND requestId = '$requestId'";
+    $resultRequestsSubmitted = mysqli_query($connection, $sqlAllRequestsSubmitted);
+    $countOfRows = mysqli_num_rows($resultReportRequest);
+    if($countOfRows == 0){
+        $submitted = 'true';
+        array_push($formatListReportRequest, $submitted);
+        print_r($formatListReportRequest);
+    }else{
+        
+        foreach($formatListReportRequest as $arr){
+            $submitted = 'false';
+            array_push($arr, $submitted);
+            echo json_encode($arr);
+        }
+        
+    }
     
 }
-echo json_encode($formatListReportRequest);
