cbb900c6c6718be14e71483e74245931d08ed41a
Johan Erdös <johan.erdos@ericsson.com>
2020-02-25 11:25:21 +0100

    thina

--- erirad/src/php/ManagerViewEmpInput.php
+++ erirad/src/php/ManagerViewEmpInput.php
@@ -4,10 +4,10 @@ require 'DBConnection.php';
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
-$empId = mysqli_escape_string($connection, $_POST['employeeId']);
+$requestId = mysqli_escape_string($connection, $_POST['requestId']);
 //$empId = '5';
 $submitted = 'true';
-$sqlGetEmpInputText = "SELECT e.employeeId, e.reportId, e.submitted, e.submittedDate FROM employeeinput e JOIN reportrequest r ON e.requestId = r.requestId WHERE e.submitted = '$submitted' AND r.authorId = '$empId'";
+$sqlGetEmpInputText = "SELECT e.employeeId, e.reportId, e.submitted, e.submittedDate FROM employeeinput e JOIN reportrequest r ON e.requestId = r.requestId WHERE e.submitted = '$submitted' AND requestId = '$requestId'";
 $resultEmpInput = mysqli_query($connection, $sqlGetEmpInputText);
 
 while($rowEmpInput = mysqli_fetch_array($resultEmpInput)){
