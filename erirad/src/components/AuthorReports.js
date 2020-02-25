22d3ec8d7ad02f64b765cf58e9a95e6fdd4fda64
Jon <jo0748gr-s@student.lu.se>
2020-02-25 16:03:21 +0100

    Fixed Searchview, removed author tab..

--- erirad/src/components/AuthorReports.js
+++ erirad/src/components/AuthorReports.js
@@ -16,89 +16,124 @@ import { red } from '@material-ui/core/colors';
 import { makeStyles } from '@material-ui/core/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SingleSnippetCard from './SingleSnippetCard';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      width: 1000,
-      maxWidth: 1000,
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-  }));
+  root: {
+    width: 1000,
+    maxWidth: 1000,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 function AuthorReports(props) {
-    const employee = useSelector(state => state.employee);
-    const [snippetTextArray, setSnippetTextArray] = useState([]);
-    const [author, setAuthor] = useState({
-        firstName: '',
-        lastName: '',
-    });
-    const handleSearch = ()=> {
-        console.log(author);
-        
+  const [snippets, setSnippets] = useState([]);
+  const employee = useSelector(state => state.employee);
+  const [snippetTextArray, setSnippetTextArray] = useState([]);
+  const [author, setAuthor] = useState({
+    firstName: '',
+    lastName: '',
+  });
+  const handleSearch = () => {
+    console.log(author);
 
-        axios({
-            method: 'post',
-            url: `http://localhost/ERIRADAPP/erirad/src/php/FilterByAuthor.php`,
-            headers: { 'content-type': 'application/json' },
-            data: author
-        })
-            .then(result => {
-                console.log(result.data)
 
-            })
-            .catch(error => console.log(error));
+    axios({
+      method: 'post',
+      url: `http://localhost/ERIRADAPP/erirad/src/php/FilterByAuthor.php`,
+      headers: { 'content-type': 'application/json' },
+      data: author
+    })
+      .then(result => {
+        console.log(result.data)
 
-    }
+        //Mappa igenom hela res.data, spara in variablerna i ny array.
+        const newArray = [];
+        result.data.map((snippet) =>
+          newArray.push({
+            text: snippet.snippetText
+          })
+        )
+        setSnippets(newArray);
+        console.log(newArray)
+      
+      })
+      .catch(error => console.log(error));
 
-    return (
-        <div className="search-column-layout">
-            <div className="homepageContainer">
-                <h2> 
-                     Har fixat så att man kan söka på author men vet ej hur man ska rendera resultaten.
-                     Finns i konsolen. 
+  }
+
+
+  return (
+    <div className="search-column-layout">
+      <div className="homepageContainer">
+        <h2>
+          Filter by author
                 </h2>
-                
-                <TextField 
-                id="outlined-multiline-static"
-                label="Employee First Name"
-                value={author.firstName} onChange={e => setAuthor({ ...author, firstName: e.target.value })}
-                multiline
-                rows="1"
-                variant="outlined"
-                />
-                <TextField 
-                id="outlined-multiline-static"
-                label="Employee Last Name"
-                value={author.lastName} onChange={e => setAuthor({ ...author, lastName: e.target.value })}
-                multiline
-                rows="1"
-                variant="outlined"
-                />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleSearch}
-                >
-                  Search
+
+        <TextField 
+       
+          id="outlined-multiline-static"
+          label="Employee First Name"
+          value={author.firstName} onChange={e => setAuthor({ ...author, firstName: e.target.value })}
+          multiline
+          rows="1"
+          cols="400"
+          variant="outlined"
+        />
+
+
+     
+        <TextField
+          id="outlined-multiline-static"
+          label="Employee Last Name"
+          value={author.lastName} onChange={e => setAuthor({ ...author, lastName: e.target.value })}
+          multiline
+          rows="1"
+          variant="outlined"
+        />
+
+
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSearch}
+        >
+          Search
                 </Button>
-            </div>
-        </div>
-    );
-    
+  
+      </div>
+
+      <div className="homepageRow">
+        {snippets.map(snippet => (
+          <div className="homepageRow">
+            <SingleSnippetCard
+              text={snippet.text}
+             
+            />
+          </div>
+        ))}
+
+      </div>
+
+
+    </div>
+  );
+
 }
 export default AuthorReports;
