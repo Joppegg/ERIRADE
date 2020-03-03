382375ac6f89a3064ad8d85b229738ba73d7b47f
Jon <jo0748gr-s@student.lu.se>
2020-03-03 12:18:07 +0100

    Fixed merge

--- erirad/src/php/ManagerViewEmpInput.php
+++ erirad/src/php/ManagerViewEmpInput.php
@@ -4,16 +4,15 @@ require 'DBConnection.php';
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
-//$requestId = mysqli_escape_string($connection, $_POST['requestId']);
+$requestId = mysqli_escape_string($connection, $_POST['requestId']);
 
-$requestId = '59';
+//$requestId = '59';
 $submitted = 'true';
 $sqlGetEmpInputReportId = "SELECT tag.tagId, tag.tagName, employeeinput.employeeId, snippet.snippetId, snippet.snippetText, employee.firstName, employee.lastName FROM reportrequest JOIN employeeinput ON employeeinput.requestId = reportrequest.requestId JOIN report ON report.reportId = employeeinput.reportId JOIN snippet ON snippet.reportId = report.reportId JOIN employee ON employee.employeeId = employeeinput.employeeId JOIN snippettag ON snippet.snippetId = snippettag.snippetId JOIN tag ON tag.tagId = snippettag.tagId WHERE reportrequest.requestId = '$requestId' AND employeeinput.submitted = '$submitted'";
 $resultEmpInputReportId = mysqli_query($connection, $sqlGetEmpInputReportId);
 while($rowEmpInput = mysqli_fetch_array($resultEmpInputReportId)){
     $list[] = $rowEmpInput;
 
-    
 
 }
 echo json_encode($list);
